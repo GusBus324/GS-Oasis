1ba64b4d8388950009a0346c98d6431c29af28d0
Gus <gsimmonds@student.saintaug.nsw.edu.au>
2025-05-14 11:36:39 +1000

    Made ai more targeted for scams not just the one scam

--- gs-oasis/query_gpt.py
+++ gs-oasis/query_gpt.py
@@ -38,7 +38,7 @@ def extract_json_from_text(text):
 def get_open_ai_repsonse(user_question):
     openai.api_key = "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"
 
-    user_prompt = "Take a look at the following message. Decide whether it is a phishing scam. Explain why it is if you believe it is."
+    user_prompt = "Take a look at the following message. Decide whether it is a possible scam threatening the user. Explain why it is if you believe it is."
 
     modifier = "Provide the response in a JSON format with the following keys: \"finalDecision\", \"rationale\", \"educationalContent\", and \"redFlags\". The finalDecision should be a clear 'Yes' or 'No' whether this is a phishing scam. The rationale should explain your reasoning. The educationalContent should be a short paragraph explaining how to identify phishing scams. The redFlags should be a list of common signs of phishing scams. IMPORTANT: Ensure your response is valid JSON that can be parsed."
 
